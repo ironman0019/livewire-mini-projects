c194e68a060ba2f849bf0a27519048b757cccae3
Emad Isavi <emadesavi@gmail.com>
2024-11-27 20:50:03 +0330

    Create livewire calculator component

---
+++ app/Livewire/Calculator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Calculator extends Component
+{
+
+    public $number1 = 0;
+    public $number2 = 0;
+    public string $action = "+";
+    public float $resault = 0;
+    public bool $disabled = false;
+
+
+    public function render()
+    {
+        return view('livewire.calculator');
+    }
+
+    public function calculate()
+    {
+        $num1 = (float)$this->number1;
+        $num2 = (float)$this->number2;
+
+        if ($this->action === "+") {
+            $this->resault = $num1 + $num2;
+
+        } else if ($this->action === "-") {
+            $this->resault = $num1 - $num2;
+
+        } else if ($this->action === "*") {
+            $this->resault = $num1 * $num2;
+
+        } else if ($this->action === "/") {
+            $this->resault = $num1 / $num2;
+
+        } else if ($this->action === "%") {
+            $this->resault = $num1 / 100 * $num2;
+        }
+    }
+
+    public function updated($property)
+    {
+        if ($this->number1 === '' || $this->number2 === '' ) {
+            $this->disabled = true;
+
+        } else {
+            $this->disabled = false;
+        }
+    }
+}
