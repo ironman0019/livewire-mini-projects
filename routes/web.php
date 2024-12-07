beb368f8da9d8dbce6ca74e37e6b2c6b62fbb96f
Emad Isavi <emadesavi@gmail.com>
2024-12-08 00:38:23 +0330

    Add routes for products, image-upload & register form livewire components

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Livewire\Calculator;
 use App\Livewire\CascadingDropdown;
+use App\Livewire\ImageUpload;
+use App\Livewire\ProductSearch;
+use App\Livewire\RegisterForm;
 use App\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +15,6 @@ Route::get('/', function () {
 Route::get('/calculator', Calculator::class)->name('calculator');
 Route::get('/todo-list', TodoList::class)->name('todo-list');
 Route::get('/cascading-dropdown', CascadingDropdown::class)->name('cascading-dropdown');
+Route::get('/products', ProductSearch::class)->name('products');
+Route::get('/image-upload', ImageUpload::class)->name('image-upload');
+Route::get('/register', RegisterForm::class)->name('register');
